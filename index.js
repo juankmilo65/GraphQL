5a1852cb7636f87521c8f8b1af9199dd78967459
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2020-07-15 01:16:04 +0300

    Seajustan los queries para que se pueda consultar por cualquier caracter .

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 require("dotenv").config();
 const { makeExecutableSchema } = require("graphql-tools");
 const express = require("express");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
+const isDev = process.env.NODE_ENV !== "production";
+console.log(process.env.NODE_ENV);
 
 // schema def.
 const typeDefs = readFileSync(
@@ -17,12 +20,14 @@ const typeDefs = readFileSync(
 );
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+app.use(cors());
+
 app.use(
   "/api",
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: isDev,
   })
 );
 
